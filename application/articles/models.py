0a990ddfa8327d49f10233fc16ba56d36c56d265
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-07-27 13:29:29 +0300

    added fields date_created and date_modified

--- application/articles/models.py
+++ application/articles/models.py
@@ -2,6 +2,8 @@ from application import db
 
 class Article(db.Model):
    id = db.Column(db.Integer, primary_key=True)
+   date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+   date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
    name = db.Column(db.String(144), nullable=False)
    writer = db.Column(db.String(144), nullable=True)
    ready = db.Column(db.Boolean, nullable=False)
