8fe04a3e48e0eabb97575c304d3498f9266b0730
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-07-27 13:59:35 +0300

    uses LoginManager to know if user has logged in and displays that information on the top of the page

--- application/__init__.py
+++ application/__init__.py
@@ -7,6 +7,7 @@ app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+# basic functionality
 from application import views
 
 from application.articles import models
@@ -15,4 +16,21 @@ from application.articles import views
 from application.auth import models
 from application.auth import views
 
+# authentication
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Login required"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# creates database tables
 db.create_all()
