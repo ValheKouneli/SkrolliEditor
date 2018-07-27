ff1c54289f9a90e1d94fbf7556a241a40999da8f
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-07-27 13:43:47 +0300

    changes database name to database

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///articles.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
@@ -13,5 +13,6 @@ from application.articles import models
 from application.articles import views
 
 from application.auth import models
+from application.auth import views
 
 db.create_all()
