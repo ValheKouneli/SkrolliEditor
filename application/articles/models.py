07cdecfd88b22bd689daa69aaa618d890ed6682b
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-07-30 13:20:19 +0300

    shows unfinished articles on the index page

--- application/articles/models.py
+++ application/articles/models.py
@@ -1,6 +1,8 @@
 from application import db
 from application.models import Base
 
+from sqlalchemy.sql import text
+
 class Article(Base):
 
    name = db.Column(db.String(144), nullable=False)
@@ -12,3 +14,16 @@ class Article(Base):
    def __init__(self, name):
       self.name = name
       self.ready = False
+
+   @staticmethod
+   def find_articles_not_ready():
+      stmt = text(""
+         "SELECT Article.id, Article.name, Article.writer FROM Article"
+         " WHERE (Article.ready = 0)")
+      res = db.engine.execute(stmt)
+
+      response = []
+      for row in res:
+         response.append({"id":row[0], "name":row[1], "writer":row[2]})
+
+      return response
