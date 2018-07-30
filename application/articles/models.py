430f380e3d4b6bca664afae522c2eb5fc01f11ed
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-07-30 12:54:14 +0300

    article and user implement abstract model Base

--- application/articles/models.py
+++ application/articles/models.py
@@ -1,10 +1,8 @@
 from application import db
+from application.models import Base
 
-class Article(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-   date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-   
+class Article(Base):
+
    name = db.Column(db.String(144), nullable=False)
    writer = db.Column(db.String(144), nullable=True)
    ready = db.Column(db.Boolean, nullable=False)
