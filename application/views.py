3aa6982a7bebe66770b334b7ffa4330410b943c0
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-08-07 23:32:37 +0300

    shows unfinished articles to non-logged in user and articles_writing for logged-in user

--- application/views.py
+++ application/views.py
@@ -5,11 +5,8 @@ from flask_login import current_user
 
 @app.route("/")
 def index():
-    unfinished_articles = Article.find_articles_not_ready()
     if current_user.is_authenticated:
-        articles_writing = current_user.articles_writing
-        return render_template("index.html", articles_writing = articles_writing)
-    if unfinished_articles.count() > 0:
-        return render_template("index.html", unfinished_articles=Article.find_articles_not_ready())
+        return render_template("index.html", logged_in=True, articles_writing = current_user.articles_writing)
     else:
-        return render_template("index.html")
+        articles = Article.find_articles_not_ready()
+        return render_template("index.html", logged_in=False, unfinished_articles = articles)
