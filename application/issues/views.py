5979a8a04bfa0f88b60da5cd519e4733800891fe
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-08-09 14:48:03 +0300

    fixes wrong address

--- application/issues/views.py
+++ application/issues/views.py
@@ -4,7 +4,7 @@ from flask_login import login_user, logout_user, current_user
 from application import app, db
 from application.issues.models import Issue
 
-@app.route("/articles/", methods=["GET"])
+@app.route("/issues/", methods=["GET"])
 def issues_index():
     issues = Issue.query.all()
     return render_template("/issues/list.html", issues = issues)
