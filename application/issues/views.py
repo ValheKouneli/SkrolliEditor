25c972b39770d2f75893dc034f34e5ebe2084126
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-08-09 14:47:23 +0300

    issues/list page

---
+++ application/issues/views.py
@@ -0,0 +1,10 @@
+from flask import render_template, request, redirect, url_for
+from flask_login import login_user, logout_user, current_user
+
+from application import app, db
+from application.issues.models import Issue
+
+@app.route("/articles/", methods=["GET"])
+def issues_index():
+    issues = Issue.query.all()
+    return render_template("/issues/list.html", issues = issues)
