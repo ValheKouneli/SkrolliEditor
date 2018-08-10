2ef82e00591f285f8e289895f78f04dcddb15bae
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-08-10 17:40:50 +0300

    makes new article form look very pretty

--- application/articles/forms.py
+++ application/articles/forms.py
@@ -7,8 +7,8 @@ class ArticleForm(FlaskForm):
         name = StringField("Article name", [validators.Length(min=1)])
         issue = SelectField("Issue", coerce=int)
         writer = SelectField("Writer", coerce=int)
-        ready = BooleanField("Done")
         editorInCharge = SelectField("Editor-in-charge", coerce=int)
+        ready = BooleanField("Done")
 
 
         class Meta:
