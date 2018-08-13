b9c0568246b27d45c6dd26c1dffff3df0a70a278
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-08-13 14:04:55 +0300

    shows one article; article list article names link to showing one article

--- application/help.py
+++ application/help.py
@@ -36,11 +36,27 @@ def getIssueOptions():
     return formatted
 
 def getArticlesWithCondition(condition="(0 = 0)"):
+    return getArticlesAmountCondition(condition=condition)
+
+# Returns an array of [amount] articles where the condition [condition]
+#  is satisfied
+def getArticlesAmountCondition(amount=0, condition="(0=0)"):
+    howmany = ""
+    if amount > 0:
+        howmany = " LIMIT %d" % int(amount)
     query = text(
-            "SELECT Article.id AS id, Article.ready AS ready, Article.name AS name, Writer.name AS writer, Editor.name AS editor_in_charge FROM Article"
-            " LEFT JOIN Account Writer ON Article.writer = Writer.id"
-            " LEFT JOIN Account Editor ON Article.editor_in_charge = Editor.id"
-            " WHERE %s" % condition +\
-            " GROUP BY Article.id"
+        "SELECT Article.id AS id, Article.ready AS ready, Article.name AS name,"
+        " Writer.name AS writer, Editor.name AS editor_in_charge FROM Article"
+        " LEFT JOIN Account Writer ON Article.writer = Writer.id"
+        " LEFT JOIN Account Editor ON Article.editor_in_charge = Editor.id"
+        " WHERE %s" % condition +\
+        " GROUP BY Article.id" + howmany
     )
     return db.engine.execute(query)
+
+def getArticleWithId(id):
+    resultArray = getArticlesAmountCondition(amount=1, condition="Article.id = %d" % id)
+    try:
+        return resultArray.first()
+    except:
+        return None
