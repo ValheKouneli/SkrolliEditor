48471626685566808f6c6fa2a92d8f6244571c2a
Valhe Kouneli <valhe.kouneli@gmail.com>
2018-08-20 16:59:21 +0300

    route to user page uses user.id, not name, because names might not be unique

--- application/people/views.py
+++ application/people/views.py
@@ -84,21 +84,20 @@ def names_create(user_id):
 
     return redirect(url_for("person_edit", user_id=user_id))
 
-@app.route("/people/<name>/", methods=["GET"])
-def show_tasks(name):
-    name_in_db = Name.query.filter_by(name = name).first()
-    if not name_in_db:
-        user = User.query.filter_by(name = name).first()
-    else:
-        user = User.query.get(name_in_db.user_id)
-    
+@app.route("/people/<user_id>/", methods=["GET"])
+def show_tasks(user_id):
+    user = User.query.get(int(user_id))
     if not user:
         return redirect(url_for("error404"))
     
+    name = user.name
+
+    articles_writing = user.get_articles_writing()
+    articles_editing = user.get_articles_editing()
 
     return render_template("people/tasks.html",
-        articles_writing = user.get_articles_writing(),
-        articles_editing = user.get_articles_editing(),
+        articles_writing = articles_writing,
+        articles_editing = articles_editing,
         posessive_form = "" + name + "'s",
         system_name = user.name,
         person_is = name + " is")
